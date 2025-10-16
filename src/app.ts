b504bbd391a9c0865c4ec5fb74c5dab9a3388417
wechuli <wechulipaul@yahoo.com>
2025-10-16 17:56:02 +0300

    feat: refactor application structure and enhance middleware
    - Updated package.json to include new dependencies: dotenv, pino, pino-http, pino-pretty, and zod.
    - Refactored app.ts to set up express application with helmet, cors, and custom middleware for request logging and error handling.
    - Enhanced index.ts to start the server and handle shutdown signals with logging.
    - Added error handling middleware to manage unexpected requests and log errors.
    - Implemented request logging middleware to capture request IDs and log request details.
    - Created envConfig.ts to manage environment variables with validation using zod.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
-export async function myApp() {
-  console.log("App is running");
-}
+import express from "express";
+import helmet from "helmet";
+import { pino } from "pino";
+import cors from "cors";
+import requestLogger from "./common/middleware/requestLogger";
+import errorHandler from "./common/middleware/errorHandler";
+
+const logger = pino({ name: "server start" });
+const app = express();
+
+// Middleware setup
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Request logging middleware
+app.use(requestLogger);
+
+// Routes
+
+// Error handling middleware
+app.use(errorHandler());
+
+export { app, logger };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-import {} from "./app";
+import { app, logger } from "./app";
+import { env } from "./common/utils/envConfig";
+
+const server = app.listen(env.PORT, () => {
+  const { NODE_ENV, HOST, PORT } = env;
+  logger.info(`Server (${NODE_ENV}) running on port http://${HOST}:${PORT}`);
+});
+
+const onCloseSignal = () => {
+  logger.info("sigint received, shutting down");
+  server.close(() => {
+    logger.info("server closed");
+    process.exit();
+  });
+  setTimeout(() => process.exit(1), 10000).unref(); // Force shutdown after 10s
+};
+
+process.on("SIGINT", onCloseSignal);
+process.on("SIGTERM", onCloseSignal);
